84d57fbc8a39d8408a5ec05a6e8aa6716a82e982
JorgeRico08 <ricoarvizujorge2002@gmail.com>
2023-03-12 19:35:51 -0600

    Update-master

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const products=require('./product')
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate=require('mongoose-findorcreate');
+const userSchema =mongoose.Schema({
+    username:{
+        type:String
+    },
+    googleid:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    photo:{
+        type:String
+    },
+    image:{
+      data:Buffer,
+      contentType:String
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    cart:[
+        {
+        item:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"products"
+        },
+        quantity:{
+            type:Number,
+            default:1
+        }
+    }],
+    orders:[{
+        type:mongoose.ObjectId,
+        ref:"orders"
+    }],
+    role:{
+        type:String,
+        default:"Customer"
+    }
+},
+    {
+        versionKey: false,
+    }
+);
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+const User= mongoose.model("users",userSchema)
+module.exports=User;
